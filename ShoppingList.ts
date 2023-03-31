b6481b3232c3cd2a569c5999f4be79fa1f2dd9bf
justmedev <ilja.ragnar@gmail.com>
2023-03-31 17:19:55 +0200

    shoppinglist fixed

--- ShoppingList.ts
+++ ShoppingList.ts
@@ -2,7 +2,7 @@
 // @ts-ignore
 // noinspection JSUnusedGlobalSymbols
 
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 export interface IShoppingListItem {
   id: string,
@@ -11,7 +11,7 @@ export interface IShoppingListItem {
 }
 
 export interface IShoppingListEntries {
-  items: IShoppingListItem[];
+  items: IShoppingListItem[]
 }
 
 export interface IShoppingList {
@@ -28,7 +28,7 @@ export interface IShoppingList {
 }
 
 interface ErrorConstructor {
-  captureStackTrace(thisArg: any, func: any): void;
+  captureStackTrace(thisArg: any, func: any): void
 }
 
 // eslint-disable-next-line max-classes-per-file
@@ -104,26 +104,18 @@ export default class ShoppingList {
    * @param description Description of the list
    * @param owner
    * @param entries (optional) Pass already existing [IShoppingListItems]{@link IShoppingListItem}.
-   * @param checkedEntries (optional) Pass already existing [IShoppingListItems]{@link IShoppingListItem}.
    */
-  constructor(name: string, description: string, owner?: string, entries?: IShoppingListItem[], checkedEntries?: IShoppingListItem[]) {
+  constructor(name: string, description: string, owner?: string, entries?: IShoppingListItem[]) {
     this.name = name;
     this.description = description;
     this.listid = uuidv4();
     this.owner = owner ?? '';
 
-    // Convert database shopping list item (IShoppingListItem) type to class type
-    if (entries) {
-      entries.forEach((t) => {
-        this.entries.push(new ShoppingListItem(t.name, t.done, t.id));
-      });
-    }
-
-    if (checkedEntries) {
-      checkedEntries.forEach((t) => {
-        this.checkedEntries.push(new ShoppingListItem(t.name, t.done, t.id));
-      });
-    }
+    // Convert database shopping list item type to class type
+    if (!entries) return;
+    entries.forEach((t) => {
+      this.entries.push(new ShoppingListItem(t.name, t.done, t.id));
+    });
   }
 
   /**
