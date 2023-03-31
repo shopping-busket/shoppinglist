8128079e9abaa59f243ee02eebd9eebc0398dd60
justmedev <ilja.ragnar@gmail.com>
2023-03-31 20:10:03 +0200

    added checkedEntries to constructor which fixes bug, where checkedEntries would not be displayed

--- ShoppingList.ts
+++ ShoppingList.ts
@@ -103,18 +103,22 @@ export default class ShoppingList {
    * @param name Name of the new list
    * @param description Description of the list
    * @param owner
-   * @param entries (optional) Pass already existing [IShoppingListItems]{@link IShoppingListItem}.
+   * @param entries (optional) Pass already existing [IShoppingListItems]
+   * {@param checkedEntries @link IShoppingListItem} (optional) Pass already existing [IShoppingListItems] (only checked)
    */
-  constructor(name: string, description: string, owner?: string, entries?: IShoppingListItem[]) {
+  constructor(name: string, description: string, owner?: string, entries?: IShoppingListItem[], checkedEntries?: IShoppingListItem[]) {
     this.name = name;
     this.description = description;
     this.listid = uuidv4();
     this.owner = owner ?? '';
 
     // Convert database shopping list item type to class type
-    if (!entries) return;
-    entries.forEach((t) => {
+    entries?.forEach((t) => {
       this.entries.push(new ShoppingListItem(t.name, t.done, t.id));
+    });
+
+    checkedEntries?.forEach((t) => {
+      this.checkedEntries.push(new ShoppingListItem(t.name, t.done, t.id));
     });
   }
 
